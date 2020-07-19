1f0157cce1babc803e8dbb91cc9b9bfce82ac96f
Asif Mahmud <idlasifmahmud@gmail.com>
2020-07-19 06:38:56 +0600

    add many thing

--- assets/js/main.js
+++ assets/js/main.js
@@ -84,7 +84,6 @@ $(document).ready(function(){
 
     $(window).on('scroll', ()=>{
         var scrollTop = $(window).scrollTop();
-        console.log(scrollTop)
         if(scrollTop > 50){
             $('.nav-wraper').css('height','80px')
             $('.header-navigation').addClass('stuck fadeIn');
@@ -95,13 +94,208 @@ $(document).ready(function(){
         }
     })
 
-    
+    /* light box */
     lightbox.option({
         'resizeDuration': 200,
         'wrapAround': true
       })
+    /* back to top */
+    $(window).scroll(()=>{
+        if( ($(window).scrollTop()) > ($(document).height()/4)){
+            $('.backtop').addClass('show');
+        }else if(($(window).scrollTop()) < ($(document).height()/4)){
+         $('.backtop').removeClass('show')
+        }
+    })
+    $('.backtop').on('click',(e)=>{
+        e.preventDefault();
+        $('html,body').animate({
+            scrollTop: 0
+        }, 700);
+    })
 
+    /* md speech */
+    $('.md-image').tilt({
+        maxTilt:10,
+        perspective:500,
+    })
+    
 
+    
+/*===================================================
+        12: Google map
+===================================================*/
+    //Google Map
+    if($('#map').length !==0){
+            
+        var googleMapSelector = $('#map');
+        var latitude = $('.google-map-wrapper').attr('data-latitude');
+        var longitude = $('.google-map-wrapper').attr('data-longitude');
+        var zoome = $('.google-map-wrapper').attr('data-zoom');
+        var zoomtoNum = Number(zoome);
+        // var mapmarker = $('.google-map-wrapper').attr('data-marker');
+        var myCenter = new google.maps.LatLng(latitude, longitude);
+
+        function initialize() {
+            var mapProp = {
+                center: myCenter,
+                zoom: zoomtoNum,
+                scrollwheel: false,
+                mapTypeId: google.maps.MapTypeId.ROADMAP,
+                styles: [
+                        {
+                            "featureType": "landscape.man_made",
+                            "elementType": "geometry",
+                            "stylers": [
+                                {
+                                    "color": "#f7f1e0"
+                                }
+                            ]
+                        },
+                        {
+                            "featureType": "landscape.natural",
+                            "elementType": "geometry",
+                            "stylers": [
+                                {
+                                    "color": "#d0e3b4"
+                                }
+                            ]
+                        },
+                        {
+                            "featureType": "landscape.natural.terrain",
+                            "elementType": "geometry",
+                            "stylers": [
+                                {
+                                    "visibility": "off"
+                                }
+                            ]
+                        },
+                        {
+                            "featureType": "poi",
+                            "elementType": "labels",
+                            "stylers": [
+                                {
+                                    "visibility": "off"
+                                }
+                            ]
+                        },
+                        {
+                            "featureType": "poi.business",
+                            "elementType": "all",
+                            "stylers": [
+                                {
+                                    "visibility": "off"
+                                }
+                            ]
+                        },
+                        {
+                            "featureType": "poi.medical",
+                            "elementType": "geometry",
+                            "stylers": [
+                                {
+                                    "color": "#fbd3da"
+                                }
+                            ]
+                        },
+                        {
+                            "featureType": "poi.park",
+                            "elementType": "geometry",
+                            "stylers": [
+                                {
+                                    "color": "#bde6ab"
+                                }
+                            ]
+                        },
+                        {
+                            "featureType": "road",
+                            "elementType": "geometry.stroke",
+                            "stylers": [
+                                {
+                                    "visibility": "off"
+                                }
+                            ]
+                        },
+                        {
+                            "featureType": "road",
+                            "elementType": "labels",
+                            "stylers": [
+                                {
+                                    "visibility": "off"
+                                }
+                            ]
+                        },
+                        {
+                            "featureType": "road.highway",
+                            "elementType": "geometry.fill",
+                            "stylers": [
+                                {
+                                    "color": "#ffe36f"
+                                }
+                            ]
+                        },
+                        {
+                            "featureType": "road.highway",
+                            "elementType": "geometry.stroke",
+                            "stylers": [
+                                {
+                                    "color": "#efd151"
+                                }
+                            ]
+                        },
+                        {
+                            "featureType": "road.arterial",
+                            "elementType": "geometry.fill",
+                            "stylers": [
+                                {
+                                    "color": "#ffffff"
+                                }
+                            ]
+                        },
+                        {
+                            "featureType": "road.local",
+                            "elementType": "geometry.fill",
+                            "stylers": [
+                                {
+                                    "color": "black"
+                                }
+                            ]
+                        },
+                        {
+                            "featureType": "transit.station.airport",
+                            "elementType": "geometry.fill",
+                            "stylers": [
+                                {
+                                    "color": "#cfb2db"
+                                }
+                            ]
+                        },
+                        {
+                            "featureType": "water",
+                            "elementType": "geometry",
+                            "stylers": [
+                                {
+                                    "color": "#a2daf2"
+                                }
+                            ]
+                        }
+                    ]
+            };
+            var map = new google.maps.Map( document.getElementById('map'), mapProp );
+            var marker = new google.maps.Marker({
+                position: myCenter,
+                // icon: mapmarker,
+            });
+            marker.setMap(map);
+
+           
+        }
+        if (googleMapSelector.length) {
+            google.maps.event.addDomListener(window, 'load', initialize);
+        }
+    }   
+        
+
+  
 
 })($)
 
